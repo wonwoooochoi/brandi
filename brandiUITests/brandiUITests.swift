1102c61bd3099fb25e2718232ac992abcb5d0113
Jack Choi <jack.choi@bunjang.co.kr>
2020-12-31 18:53:23 +0900

    test: 검색 테스트.

--- brandiUITests/brandiUITests.swift
+++ brandiUITests/brandiUITests.swift
@@ -16,21 +16,28 @@ class brandiUITests: XCTestCase {
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+		let app = XCUIApplication()
+		app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+	
+	func testInputText() throws {
+		
+		let app = XCUIApplication()
+		
+		let searchBar = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .searchField).element
+		searchBar.tap()
+		searchBar.typeText("bts")
+		
+		_ = searchBar.waitForExistence(timeout: 10000000)
+		searchBar.tap()
+		searchBar.typeText("apple")
+		
+	}
+	
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
