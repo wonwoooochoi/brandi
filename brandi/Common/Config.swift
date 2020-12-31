d821ea1afe79591ecb80d113b239b5cce3168dee
Jack Choi <jack.choi@bunjang.co.kr>
2020-12-31 15:55:20 +0900

    feat: Config 생성.

---
+++ brandi/Common/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  brandi
+//
+//  Created by Wonwoo Choi on 2020/12/31.
+//
+
+import Foundation
+
+struct Config {
+	static let apiKey = "KakaoAK 74ecf9300b502339cb6284e894a5df59"
+}
