91cb40ae0ea622059dc55a0412d8972aadb5c94f
Jack Choi <jack.choi@bunjang.co.kr>
2020-12-31 18:10:01 +0900

    feat: UICollectionView 익스텐션 추가.

---
+++ brandi/Common/Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Extension.swift
+//  brandi
+//
+//  Created by Wonwoo Choi on 2020/12/31.
+//
+
+import Foundation
+import UIKit
+
+extension UICollectionView {
+	
+	func dequeueReusableCell<T: UICollectionViewCell>(_: T.Type, for indexPath: IndexPath) -> T {
+		let name = String(describing: T.self)
+		guard let cell = dequeueReusableCell(withReuseIdentifier: name, for: indexPath) as? T else {
+			fatalError("dequeue fail: '\(name)'")
+		}
+		return cell
+	}
+	
+	func register<T: UICollectionViewCell>(_: T.Type) {
+		let name = String(describing: T.self)
+		register(UINib(nibName: name, bundle: nil), forCellWithReuseIdentifier: name)
+	}
+	
+}
