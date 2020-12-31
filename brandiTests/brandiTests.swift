814b3f673628d10b94d94206ab3766210ac6ee12
Jack Choi <jack.choi@bunjang.co.kr>
2020-12-31 16:06:48 +0900

    test: API 테스트.

--- brandiTests/brandiTests.swift
+++ brandiTests/brandiTests.swift
@@ -9,7 +9,23 @@ import XCTest
 @testable import brandi
 
 class brandiTests: XCTestCase {
-
+	
+	func testSearchAPI() throws {
+		
+		let expectation = self.expectation(description: "wait networking...")
+		
+		request(keyword: "bts", page: 2) { 
+			expectation.fulfill()
+			XCTAssertNotNil($0)
+		} failure: {
+			expectation.fulfill()
+			XCTFail($0.localizedDescription)
+		}
+		
+		waitForExpectations(timeout: 5.0, handler: nil)		
+		
+	}
+	
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,4 +46,49 @@ class brandiTests: XCTestCase {
         }
     }
 
+}
+
+
+extension brandiTests {
+	
+	func request(keyword: String,
+				 page: Int,
+				 success:((Items) -> Void)? = nil,
+				 failure: ((Error) -> Void)? = nil) -> Void {
+		
+		let urlString = "https://dapi.kakao.com/v2/search/image"
+		var urlComponents = URLComponents(string: urlString)!
+		urlComponents.queryItems = [
+			URLQueryItem(name: "query", value: keyword),
+			URLQueryItem(name: "page", value: String(page)),
+			URLQueryItem(name: "size", value: "30")
+		]
+		
+		var request = URLRequest(url: urlComponents.url!)
+		request.httpMethod = "GET"
+		request.setValue(Config.apiKey, forHTTPHeaderField: "Authorization")
+		
+		let task = URLSession.shared.dataTask(with: request) { data, response, error in
+						
+			if let error = error {
+				failure?(error)
+			}
+			else if let data = data {
+				
+				guard let items = try? JSONDecoder().decode(Items.self, from: data)
+				else { failure?(RequestError.parse); return }
+				
+				success?(items)
+				
+			}
+			else {
+				failure?(RequestError.request)
+			}
+			
+		}
+		
+		task.resume()
+		
+	}
+	
 }
